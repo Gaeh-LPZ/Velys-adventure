91d3c8517c18b8a16a7c94ae1fbf87f6ca2d7fe6
Gaeh-LPZ <galob567@gmail.com>
2025-12-15 17:19:44 -0600

    config page created

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+// Declaracion de variables
+const btnConfig = document.querySelector('#config');
+
+// Funciones y metodos
 function playAudio(){
     document.getElementById('audio-fondo').play();
 }
